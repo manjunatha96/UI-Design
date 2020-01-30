84c9d5855d71e5f1a0d3500d1fd1c0637a291d49
Manjunatha <58422091+manjunatha96@users.noreply.github.com>
2020-01-30 10:22:55 +0530

    first table and nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeatilsComponent } from './Components/deatils/deatils.component';
+import { InnerComponent } from './Components/inner/inner.component';
+import { TableCheckboxComponent } from './Components/table-checkbox/table-checkbox.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'Deatils', component:DeatilsComponent},
+  {path:'inner', component:InnerComponent},
+  {path:'checkbox', component:TableCheckboxComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeatilsComponent } from './Components/deatils/deatils.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatFormFieldModule, MatTableModule, MatInputModule, MatCheckboxModule } from  '@angular/material';
+import { InnerComponent } from './Components/inner/inner.component';
+import { TableCheckboxComponent } from './Components/table-checkbox/table-checkbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeatilsComponent,
+    InnerComponent,
+    TableCheckboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatCheckboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
